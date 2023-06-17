6b44373a5c24b90bdbde9c6bc526978caff2fefa
abbas <abbasmomeny1994@gmail.com>
2023-06-17 09:50:16 +0330

    feat: add user_name,user_first_name , user_last_name key

--- config/authenticate.php
+++ config/authenticate.php
@@ -1,4 +1,7 @@
 <?php
 return [
-    'user_id' => 'X-Consumer-ID'
+    'user_id'         => 'X-Consumer-ID',
+    'user_name'       => 'X-Consumer-Name',
+    'user_first_name' => 'X-Consumer-Firstname',
+    'user_last_name'  => 'X-Consumer-Lastname',
 ];
